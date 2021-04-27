de03f36a8ed4f178a8d27c09ed001b4177567cd2
Sergius Manolov <67722057+sergim1423@users.noreply.github.com>
2021-04-27 03:15:34 -0400

    Update SpaceProject.py
    trying to create bullets, they are not getting produced on the screen idk why

--- SpaceProject.py
+++ SpaceProject.py
@@ -8,10 +8,28 @@
 import pygame
 pygame.init()
 
+screen = pygame.display.set_mode([650, 650])
+x= 325
+y=0
+image=pygame.image.load(r'.\space backround.jpg')
+
+class player(object):
+    def __init__(self,x,y,width,height):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+        self.velocity=8
+
+
 class cordinate():
     def __init__(self, xCord, yCord):
         self.xCord = xCord
         self.yCord = yCord
+
+    def draw(self,screen):
+            screen.blit(character1,(spaceship1Cordinate.xCord,spaceship1Cordinate.yCord))
+            screen.blit(character2)
 
 #class called projectiles to hold the bullets that
 #will come out of the ship
@@ -23,8 +41,15 @@ class projectiles(object):
         self.color=color
         self.velocity=8
 
-    def draw(win):
-        pygame.draw.circle(win,self.color(self.x,self.y), self.radius)
+    def draw(self,screen):
+        pygame.draw.circle(screen,self.color(self.x,self.y), self.radius)
+
+def redrawGameWindow():
+    bullets=[]
+        #screen.blit(image(0,0))
+    for bullet in bullets:
+        bullet.draw(screen)
+    pygame.display.update()
 
 def player1keyMove(spaceship1Cordinate):
     pressed = pygame.key.get_pressed()
@@ -38,7 +63,11 @@ def player1keyMove(spaceship1Cordinate):
         spaceship1Cordinate.xCord +=4
 
 def player2keyMove(spaceship2Cordinate):
+    bullets = []
     pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_SPACE]:
+        if len(bullets)<5:
+            bullets.append(projectiles(30,20,8,(255,255,255)))
     if pressed[pygame.K_w]:
         spaceship2Cordinate.yCord -=4
     if pressed[pygame.K_s]:
@@ -49,6 +78,7 @@ def player2keyMove(spaceship2Cordinate):
         spaceship2Cordinate.xCord +=4
 
 def main():
+    bullets= []
 # Set up the drawing window
     screen = pygame.display.set_mode([650, 650])
     x= 325
@@ -66,7 +96,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 clock.tick(60)
-
+        for bullet in bullets:
+            if bullet.bulletX < 650 and bullet.bulletX> 0:
+                bullet.bulletX += bullet.velocity
+            else:
+                bullets.pop(bullets.index(bullet))
+        
         # set backround as image from the web
         screen.fill((255, 2, 255))
         image=pygame.image.load(r'.\space backround.jpg')
@@ -86,8 +121,7 @@ def main():
 
         player1keyMove(spaceship1Cordinate)
         player2keyMove(spaceship2Cordinate)
-
-
+        redrawGameWindow()
         # Flip the display
         pygame.display.flip()
 
